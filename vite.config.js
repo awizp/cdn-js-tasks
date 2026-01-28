8bd24b3fe8b9bb7aebe864b15c33f42b903a5258
Awizp <ridvishnu@gmail.com>
2026-01-28 23:19:47 +0530

    fix: configuration of index file fixed

--- vite.config.js
+++ vite.config.js
@@ -29,8 +29,6 @@ export default defineConfig({
                 task16: resolve(__dirname, '/components/task16/index.html'),
                 task17: resolve(__dirname, '/components/task17/index.html'),
                 task18: resolve(__dirname, '/components/task18/index.html'),
-                task19: resolve(__dirname, '/components/task19/index.html'),
-                task20: resolve(__dirname, '/components/task20/index.html'),
             }
         }
     }
